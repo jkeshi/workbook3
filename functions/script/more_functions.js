0443d40cc09af3814e91c65cb568b648562f476e
jkesh <jkeshi@gmail.com>
2022-10-03 14:54:14 -0400

    finished 1 and half

---
+++ functions/script/more_functions.js
@@ -0,0 +1,22 @@
+function displayMailingLabel(name, address, city, state, zip) {
+  console.log(name);
+  console.log(address);
+  console.log(city);
+  console.log(state);
+  console.log(zip);
+}
+let name = "google";
+let address = "134 Ottley Dr Ne #100";
+let city = "Atlanta";
+let state = "Georgia";
+let zip = "30324";
+displayMailingLabel("google","134 ottley Dr NE #100","Atlanta","Georgia","30324");
+
+function addNumbers(num1, num2) {
+  console.log(`${num1} + ${num2}= ${parseFloat(num1) + parseFloat(num2)});
+}
+addNumbers("6","6"); 
+
+function displayReceipt(totalDue,amountPaid) {
+ 
+}
