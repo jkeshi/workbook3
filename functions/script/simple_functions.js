ca0fb8aa76b126bc1fe8080a49ad47416dc3fc89
jkesh <jkeshi@gmail.com>
2022-10-03 13:04:05 -0400

    update wrk3

---
+++ functions/script/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+  console.log("dance");
+  console.log("listen to music");
+}
+favoriteThings();
+
+function whyImHere() {
+  console.log("I want to succeed");
+  console.log("My family");
+}
+whyImHere();
+
+function favoritePlace(params) {
+  console.log("Masai Mara");
+}
+
+favoritePlace();
